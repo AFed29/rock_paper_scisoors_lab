ac55496a188be5651d248bdcebb3533e25e87391
Miguel Moreno Baladron <miguel.moreno.baladron@gmail.com>
2018-02-20 14:16:05 +0000

    Fixed indentation in game class

--- models/game.rb
+++ models/game.rb
@@ -9,14 +9,14 @@ class Game
     when @hand1 == @hand2
       return "it's a draw!"
     when (
-          (@hand1 == "paper" && @hand2 == "rock") ||
-          (@hand1 == "rock" && @hand2 == "scissors") ||
-          (@hand1 == "scissors" && @hand2 == "paper")
-        )
-      return "#{@hand1} wins!"
-    else
-      return "#{@hand2} wins!"
-    end
+      (@hand1 == "paper" && @hand2 == "rock") ||
+      (@hand1 == "rock" && @hand2 == "scissors") ||
+      (@hand1 == "scissors" && @hand2 == "paper")
+    )
+    return "#{@hand1} wins!"
+  else
+    return "#{@hand2} wins!"
   end
+end
 
 end
